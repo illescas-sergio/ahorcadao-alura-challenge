71671b8064b45dd6ca6702c83cbe41c6526fa46e
Sergio <illescas.s.m@gmail.com>
2022-06-21 01:01:32 -0300

    funcionalidades ok

--- js/botonesDesarrolloJuego.js
+++ js/botonesDesarrolloJuego.js
@@ -9,6 +9,32 @@ botonNuevoJuego.addEventListener('click', function(){
     }
 
     ahorcado();
+});
+
+let botonDesistir = document.getElementById('boton-juego-desistir');
+
+botonDesistir.addEventListener('click', function(){
+    console.log('click en desistir')
+    deshabilitarTeclas();
+    
+    let arrSecreta = palabraSecreta.split('');
+    let arrCorrecta = palabraCorrecta.split('');
+    
+    let dif = arrSecreta.filter(el => !arrCorrecta.includes(el));
+   
+    dif.forEach(function(el){
+        
+            if(palabraSecreta.includes(el)){
+                adicionarLetraCorrecta(palabraSecreta.indexOf(el));
+                for(let i = 0; i < palabraSecreta.length; i++){
+                    if(palabraSecreta[i] === el){
+                    escribirLetraCorrecta(i)
+                    }
+                }
+                        
+            } 
+        
+    })
 })
 
 
